5d9c00dac569e059f40fd019df51b8f2e275f097
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-01-27 09:32:16 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    while(true) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0 && b == 0) 
+            break;
+        
+        if (a > b) {
+            cout << "Yes" << '\n';
+        } else 
+            cout << "No" << '\n';
+
+
+    }
+}
