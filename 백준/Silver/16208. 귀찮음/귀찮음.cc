ceb5f1f198d8958cbed4b81bd2225843495fb2d1
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-06 21:44:43 +0900

    [Silver V] Title: 귀찮음, Time: 44 ms, Memory: 3920 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.cc
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -4,26 +4,28 @@
 using namespace std;
 
 int main() {
-	// 코드 작성
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false); 
+    cin.tie(0); 
+    cout.tie(0);
 
-	int n; cin >> n;
-	int total_length = 0;
+	int n; 
+    cin >> n;
 	vector<int> len(n);
 	
+    long long total_length = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> len[i];
 		total_length += len[i];
 	}
 
-	sort(len.begin(), len.end());
+	sort(len.begin(), len.end()); 
 
-	int price = 0;
-	for (int i = 0; i < n-1; i++) {
+	long long price = 0;
+	for (int i = 0; i < n-1; i++) { 
 		total_length -= len[i];
-		price += len[i] * total_length;
+		price += (long long)len[i] * total_length; 
 	}
-	cout << price;
 
+	cout << price << '\n'; 
 	return 0;
 }
