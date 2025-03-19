7eac199a1325620ed19f1367ed46897888f302b4
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-20 00:55:54 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	string str; cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 65 && str[i] <= 90) {
+			str[i] += 32;
+		}
+		else {
+			str[i] -= 32;
+		}
+	}
+	cout << str;
+}
