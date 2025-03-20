e236829de7c9d8b8e287c6e320961312e3a03a19
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-21 00:36:43 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10189. Hook/Hook.cc
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -1,11 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+int main(void) {
     cout << "#  # #### #### #  #" << '\n';
     cout << "#### #  # #  # # #" << '\n';
     cout << "#### #  # #  # # #" << '\n';
