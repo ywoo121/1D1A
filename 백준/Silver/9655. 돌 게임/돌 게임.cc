002606da452d1edf71dfab491dac418c1a796ff5
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-22 14:57:29 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -5,38 +5,9 @@ int main(void) {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     
     int n; cin >> n;
-    int remain = n; // 남은 거
 
-    while (remain >= 0) {
-        // 상근
-        if (remain >= 3) {
-            remain -= 3;
-            if (remain == 0) {
-                cout << "SK";
-                break;
-            }
-        } else {
-            remain -= 1;
-            if (remain == 0) {
-                cout << "SK";
-                break;
-            }
-        }
-
-        // 창영
-        if (remain >= 3) {
-            remain -= 3;
-            if (remain == 0) {
-                cout << "CY";
-                break;
-            }
-        } else {
-            remain -= 1;
-            if (remain == 0) {
-                cout << "CY";
-                break;
-            }
-        }
-
-    }
-}
+    if (n % 2 == 0) {
+        cout << "CY";
+    } else
+        cout << "SK";     
+ }
