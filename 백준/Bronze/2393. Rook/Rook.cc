1556341798222c920737f29413ab645acbe6667f
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-23 15:36:45 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cout.tie(0); cin.tie(0);
+
+    cout << "  ___  ___  ___" << '\n';
+    cout << "  | |__| |__| |" << '\n';
+    cout << "  |           |" << '\n';
+    cout << "   \\_________/" << '\n';
+    cout << "    \\_______/" << '\n';
+    cout << "     |     |" << '\n';
+    cout << "     |     |" << '\n';
+    cout << "     |     |" << '\n';
+    cout << "     |     |" << '\n';
+    cout << "     |_____|" << '\n';
+    cout << "  __/       \\__" << '\n';
+    cout << " /             \\" << '\n';
+    cout << "/_______________\\" << '\n';
+    
+    return 0;
+}
