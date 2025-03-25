5a62c15ddb77e2c8dcc0301306cd0112b7cefb67
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-03-25 23:45:27 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+   long long n, m; cin >> n >> m;
+   if (n == m) {
+    cout << "1";
+   } else 
+    cout << "0";
+}
