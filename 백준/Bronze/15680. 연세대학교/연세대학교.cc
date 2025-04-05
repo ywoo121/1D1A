0c21ec0d27258b5f43f89dc62c0fe0e1ec8738a7
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-04-05 21:30:51 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+
+    if (n == 0) {
+        cout << "YONSEI";
+    } else
+    {
+        cout << "Leading the Way to the Future";
+    }
+    
+}
