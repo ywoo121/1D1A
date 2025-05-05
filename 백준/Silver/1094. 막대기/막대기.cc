55305139c56775d8ae0ace6e21c46237c01b5344
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-05-06 02:06:11 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int x; cin >> x;
+    int ans = 0;
+
+    while (x > 0) {
+        if (x % 2 == 1) {
+            ans++;
+        }
+        x /= 2;
+    }
+ 
+    cout << ans;
+}
