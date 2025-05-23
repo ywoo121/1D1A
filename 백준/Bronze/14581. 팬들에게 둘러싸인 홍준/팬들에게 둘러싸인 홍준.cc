235868eb45bdca010b4d83c90035fae898196607
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-05-24 03:56:38 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string fan; cin >> fan;
+
+    cout << ":fan::fan::fan:"<< '\n';
+    cout << ":fan::"<<fan<<"::fan:"<< '\n';
+    cout << ":fan::fan::fan:"<< '\n';
+
+}
