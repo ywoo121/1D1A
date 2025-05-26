2550c3b7e1f002ea1b7a2c48cdff114bbc5e11b8
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-05-27 03:25:53 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int r1, s; cin >> r1 >> s;
+
+    cout << s * 2 - r1;
+}
