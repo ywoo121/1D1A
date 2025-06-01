6f90169cd21b60720aa77ddea090f257a79efb79
yeonuu <71367047+ywoo121@users.noreply.github.com>
2025-06-01 22:37:00 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int n, min = 100, sum = 0;
+ 
+	for (int i = 0; i < 7; i++) {
+		cin >> n;
+		if (n % 2 == 1) {
+			sum += n;
+			if (n < min) 
+                min = n;
+		}
+	}
+	if (sum == 0) 
+        cout << -1;
+	else {
+        cout << sum << '\n';   
+        cout << min;
+    }
+	return 0;
+}
