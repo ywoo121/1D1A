ddcd6638e9d803c718c37cc9b899819b9504ef37
yeonuu <71367047+ywoo121@users.noreply.github.com>
2025-07-02 05:59:58 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >> y;
+
+    if (2018 <= y && y <= 2021) {
+        cout << "A" << endl;
+    } else if (y == 2022) {
+        cout << "A" << endl;
+    }
+
+    return 0;
+}
