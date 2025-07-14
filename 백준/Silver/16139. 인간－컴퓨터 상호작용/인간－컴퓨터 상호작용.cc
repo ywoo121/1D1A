825d21c080857ce2901f3d18e53d85f9a7ab7d78
yeonuu <71367047+ywoo121@users.noreply.github.com>
2025-07-14 19:10:50 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 100 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,28 +1,34 @@
 #include <iostream>
 using namespace std;
 
-string str;      // 문자열 S
-
-void sol(char a, int l, int r) {
-    int cnt = 0;
-    for (int i = l; i <= r; i++) {
-        if (str[i] == a) {
-            cnt++;
-        }
-    }
-    cout << cnt << '\n';
-}
+int arr[26][200001];
 
 int main(void) {
-    ios::sync_with_stdio(false); 
-    cin >> str;
-    int q; cin >> q;            // 질문의 수 q
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    string str; cin >> str;
+    int q; cin >> q;
+
+
+    int size = str.size();
+    
+    // 누적합 구하기
+    for (int i = 0; i < size; i++) {
+        int a = str[i] - 'a';
+
+        for (int j = 0; j < 26; j++) {
+            arr[j][i + 1] = arr[j][i]; 
+        }
+        arr[a][i + 1]++;  
+    }
 
     for (int i = 0; i < q; i++) {
-        char a; cin >> a;
-        int l; cin >> l;
-        int r; cin >> r;
+        char a; int l, r;
+        cin >> a >> l >> r;
 
-        sol(a, l, r);
+        int c = a - 'a';
+
+        // 횟수 계산
+        cout << arr[c][r+1] - arr[c][l] << '\n';
     }
 }
