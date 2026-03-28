cca4bac77db3f8184062c9d773108c3056a35748
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-03-28 10:34:11 +0900

    [Silver II] Title: 스택 수열, Time: 16 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,46 +1,46 @@
-#include <iostream>
-#include <stack>
-#include <vector>
+//  스택 수열
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int n, input, num = 1;
-    stack<int> s;
-    vector<char> c;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> input;
+    int n;
+    cin >> n;
 
-		if (!s.empty() && s.top() == input)
-		{
-			s.pop();
-			c.push_back('-');
-		}
-		else if (num <= input)
-		{
-			while (num <= input)
-			{
-				s.push(num++);
-				c.push_back('+');
-			}
+    stack<int> st;
+    vector<char> res;
 
-			s.pop();
-			c.push_back('-');
-		}
-		else if (!s.empty() && s.top() > input)
-		{
-			cout << "NO";
-			return 0;
-		}
-	}
+    // 다음에 push할 숫자
+    int nextNum = 1;
 
-	for (auto a : c)
-	{
-		cout << a << "\n";
-	}
-	return 0;
+    for (int i = 0; i < n; i++) {
+        int target;
+        cin >> target;
+
+        // target이 나올 때까지 push
+        while (nextNum <= target) {
+            st.push(nextNum++);
+            res.push_back('+');
+        }
+
+        // top이 target이면 pop
+        if (!st.empty() && st.top() == target) {
+            st.pop();
+            res.push_back('-');
+        } else {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    for (char op: res) {
+        cout << op << '\n';
+    }
+
+    return 0;
 }
