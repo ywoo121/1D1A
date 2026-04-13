fe446ff149924b8b4248d455c9081c06c8ac43ff
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-04-14 00:43:26 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,32 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+//  안녕
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int N; cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin >> N;
 
-    vector<int> L(N); 
-    vector<int> J(N); 
+    vector<int> L(N);
+    vector<int> J(N);
 
     // 입력받기
-    for (int i = 0; i < N; ++i) cin >> L[i];
-    for (int i = 0; i < N; ++i) cin >> J[i];
+    for (int i = 0; i < N; i++) cin >> L[i];
+    for (int i = 0; i < N; i++) cin >> J[i];
 
-    vector<int> dp(101, 0); 
+    vector<int> dp(101, 0);
 
-    for (int i = 0; i < N; ++i) {
+    // dp 배열 채우기
+    for (int i = 0; i < N; i++) {
         for (int j = 100; j >= L[i]; --j) {
             dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
         }
     }
 
-    int maxJoy = 0;
-    for (int i = 1; i < 100; ++i) {
-        maxJoy = max(maxJoy, dp[i]);
+    int ans = 0;
+    for (int i = 1; i < 100; i++) {
+        ans = max(ans, dp[i]);
     }
 
-    cout << maxJoy << '\n';
-    return 0;
+    cout << ans << '\n';
 }
