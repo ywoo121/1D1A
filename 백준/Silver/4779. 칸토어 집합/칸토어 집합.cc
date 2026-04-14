f5d308c1ceeaef5b0e6484cd8b8851582217d6dd
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-04-15 00:10:59 +0900

    [Silver III] Title: 칸토어 집합, Time: 104 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -1,33 +1,32 @@
-#include <iostream>
-#include <cmath>
-#include <string>
+//  칸토어 집합
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
-// 재귀적으로 가운데 공백을 만드는 함수
-void makeCantor(string &s, int start, int end) {
-    if (end - start < 3) return;
-
-    int third = (end - start) / 3;
-
-    // 가운데 구간을 공백으로 설정
-    for (int i = start + third; i < start + 2 * third; i++) {
-        s[i] = ' ';
+void sol(int n) {
+    if (n == 0) {
+        cout << "-";
+        return;
     }
 
-    // 좌, 우 구간에 대해 재귀 호출
-    makeCantor(s, start, start + third);
-    makeCantor(s, start + 2 * third, end);
+    sol(n - 1);
+    for (int i = 0; i < pow(3, n - 1); i++) {
+        cout << " ";
+    }
+    sol(n - 1);
 }
 
-int main() {
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     int n;
     while (cin >> n) {
-        int len = pow(3, n);      // 총 길이
-        string result(len, '-');  // 전부 '-'로 초기화
-
-        makeCantor(result, 0, len);  // 가운데 공백을 넣기 위한 재귀
-
-        cout << result << '\n';   // 최종 문자열 출력
+        sol(n);
+        cout << "\n";
     }
     return 0;
 }
+
