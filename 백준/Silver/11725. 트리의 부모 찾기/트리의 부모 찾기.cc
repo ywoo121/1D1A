ffc5b9c3facd73cc9c82abaec9b270a1cdd5e891
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-04-21 00:39:55 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,33 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+//  트리의 부모 찾기
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
 static int n;
-static vector<vector<int>> v;   // 트리 정보 저장
+static vector<vector<int> > v; // 트리 정보 저장
 static vector<bool> visited;
 static vector<int> ans;
 void DFS(int num);
 
 int main(void) {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n;
-    
-    v.resize(n+1);
-    visited.resize(n+1);
+
+    v.resize(n + 1);
+    visited.resize(n + 1);
     fill(visited.begin(), visited.end(), false);
-    ans.resize(n+1);
-    
-    for (int i = 1; i < n; i++) {      // n-1개의 줄에 받으니까...!
-        int s, e; cin >> s >> e;
+    ans.resize(n + 1);
+
+    for (int i = 1; i < n; i++) {
+        // n-1개의 줄에 받으니까...!
+        int s, e;
+        cin >> s >> e;
         v[s].push_back(e);
         v[e].push_back(s);
     }
-    
+
     DFS(1);
-    
     for (int i = 2; i <= n; i++) {
         cout << ans[i] << '\n';
     }
@@ -35,10 +38,10 @@ int main(void) {
 
 void DFS(int num) {
     visited[num] = true;
-    
+
     for (int i: v[num]) {
         if (visited[i] == false) {
-            ans[i] = num;   // 부모 노드를 저장하기
+            ans[i] = num; // 부모 노드를 저장하기
             DFS(i);
         }
     }
