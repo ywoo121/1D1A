13954c211334bbfcfa0a8a4b4c01ddf32ea06de7
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-04-27 21:18:43 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,33 +1,34 @@
-#include <iostream>
-#include <queue>
+//  최대 힙
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
-priority_queue<int> pq;
-vector<int> v;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-int  main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	int n; cin >> n;
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		if (a == 0) {
-			if (pq.empty()) {
-				v.push_back(0);
-			}
-			else {
-				v.push_back(pq.top());
-				pq.pop();
-			}
-		}
-		else {
-			pq.push(a);
-		}
-	}
-	for (int j = 0; j < v.size(); j++) {
-		cout << v[j] << '\n';
+    priority_queue<int> pq;
 
+    while (N--) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
     }
-	return 0;
+
+    return 0;
 }
