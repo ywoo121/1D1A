1ad82a017b92986d021fb8948ad71e67ed423398
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-06-19 23:04:35 +0900

    [level 2] Title: 피보나치 수, Time: 0.32 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    
+    int a = 0;
+    int b = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        int c = (a + b) % 1234567;
+        a = b;
+        b = c;
+    }
+    
+    return b;
+}
