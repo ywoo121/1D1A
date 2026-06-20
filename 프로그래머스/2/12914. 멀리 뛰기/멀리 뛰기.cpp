fbbd28dbe8d7b3060673e9d1c2d7cdc517f44b94
yeonuu <71367047+ywoo121@users.noreply.github.com>
2026-06-21 00:07:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    vector<long long> dp(n + 2, 0);
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+
+    return dp[n];
+}
